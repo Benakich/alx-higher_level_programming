62d19bc6d8c03f0778a2ba7ea86fd84a1bdbace8
Benakich <Benakich@gmail.com>
2022-12-07 23:36:29 +0000

    updates

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for s in dir(hidden_4):
-        if s[0:2] != "__":
-            print(s)
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
